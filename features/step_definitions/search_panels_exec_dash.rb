4d9fe9b5003f7d1dc55f4a654fe8f87bbb2360e3
David Jimenez <david.calvo@clavistechnology.com>
2015-02-25 09:55:46 +0000

    EUI-1366 Improve performance when the user logging in
    - Remove double quotes

--- features/step_definitions/search_panels_exec_dash.rb
+++ features/step_definitions/search_panels_exec_dash.rb
@@ -1,18 +1,18 @@
 Then(/^I go to the exec dash$/) do
-  click_link "Executive Dashboard"
+  click_link 'Executive Dashboard'
   verify_loading_mask_hidden 25
 end
 
 Given(/^I configure search panels to be displayed$/) do
   # noinspection RubyResolve
   jsToRun = <<-eos
-    var searchReport = _.findWhere(ClavisCustomer.reports, {name: "Search"});
+    var searchReport = _.findWhere(ClavisCustomer.reports, {name: 'Search'});
     var searchReportConfigs = searchReport.configuration;
-    var displaySearchScoresPanelConfig = _.findWhere(searchReportConfigs, {name: "Display Search Scores Panel"});
-    displaySearchScoresPanelConfig.value = "1";
+    var displaySearchScoresPanelConfig = _.findWhere(searchReportConfigs, {name: 'Display Search Scores Panel'});
+    displaySearchScoresPanelConfig.value = '1';
 
-    var displayShareOfSearchPanelConfig = _.findWhere(searchReportConfigs, {name: "Display Share of Search Panel"});
-    displayShareOfSearchPanelConfig.value = "1";
+    var displayShareOfSearchPanelConfig = _.findWhere(searchReportConfigs, {name: 'Display Share of Search Panel'});
+    displayShareOfSearchPanelConfig.value = '1';
   eos
 
   # noinspection RubyResolve
@@ -20,19 +20,19 @@ Given(/^I configure search panels to be displayed$/) do
 end
 
 Then(/^I should see the search panels$/) do
-  page.should have_content("Search Ranking - Search Scores")
-  page.should have_content("Search Ranking - Share of Search")
+  page.should have_content('Search Ranking - Search Scores')
+  page.should have_content('Search Ranking - Share of Search')
 end
 
 Given(/^I configure search panels to be hidden$/) do
   jsToRun = <<-eos
-    var searchReport = _.findWhere(ClavisCustomer.reports, {name: "Search"});
+    var searchReport = _.findWhere(ClavisCustomer.reports, {name: 'Search'});
     var searchReportConfigs = searchReport.configuration;
-    var displaySearchScoresPanelConfig = _.findWhere(searchReportConfigs, {name: "Display Search Scores Panel"});
-    displaySearchScoresPanelConfig.value = "0";
+    var displaySearchScoresPanelConfig = _.findWhere(searchReportConfigs, {name: 'Display Search Scores Panel'});
+    displaySearchScoresPanelConfig.value = '0';
 
-    var displayShareOfSearchPanelConfig = _.findWhere(searchReportConfigs, {name: "Display Share of Search Panel"});
-    displayShareOfSearchPanelConfig.value = "0";
+    var displayShareOfSearchPanelConfig = _.findWhere(searchReportConfigs, {name: 'Display Share of Search Panel'});
+    displayShareOfSearchPanelConfig.value = '0';
   eos
 
   # noinspection RubyResolve
@@ -40,6 +40,6 @@ Given(/^I configure search panels to be hidden$/) do
 end
 
 Then(/^I should not see the search panels$/) do
-  page.should_not have_content("Search Ranking - Search Scores")
-  page.should_not have_content("Search Ranking - Share of Search")
+  page.should_not have_content('Search Ranking - Search Scores')
+  page.should_not have_content('Search Ranking - Share of Search')
 end
